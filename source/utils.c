8ed2dc7324960928eaf7a11aa0b64cb35086c601
Francesco Bottalico <francesco2031@outlook.it>
2025-01-30 16:18:04 +0100

    Add string to image rendering capabilities

--- source/utils.c
+++ source/utils.c
@@ -1,4 +1,6 @@
 #include "utils.h"
+#include "font.h"
+#include <string.h>
 #include <errno.h>
 
 u16 convert_endian16(u16 value)
@@ -40,7 +42,7 @@ void decode_string(char *out, const u16 *in)
 	u8 i;
 	u16 cur_char;
 
-	// both out and have size 8
+	// both in and out have size 8
 	for (i = 0; in[i] != 0xFFFF; i++) {
 		cur_char = in[i];
 		// TODO add other characters
@@ -54,4 +56,55 @@ void decode_string(char *out, const u16 *in)
 			out[i] = '?';
 	}
 	out[i] = 0;
+}
+
+void render_string(void *dst, const u8 width, const char *str, u16 x_offset, s16 y_offset, u8 color)
+{
+	u16 *buf = (u16 *) dst;
+	u32 pixdata;
+
+	for (u8 i = 0; str[i]; i++) {
+		for (u8 c = 0; c < font_width[str[i] - 0x20]; c++) {
+			pixdata = font[str[i] - 0x20][c] | font[str[i] - 0x20][c + 6] << 8;
+
+			// shift on y axis
+			if (y_offset > 0)
+				pixdata <<= y_offset;
+			else if (y_offset < 0)
+				pixdata >>= -y_offset;
+
+			pixdata = (pixdata << 8 | (pixdata & 0xFFFF)) & 0xFF00FF;
+
+			// 1bpp to 2bpp conversion
+			switch (color) {
+				case 1:
+					pixdata = pixdata << 8;
+					break;
+				case 3:
+					pixdata = pixdata << 8 | pixdata;
+					break;
+			}
+
+			buf[x_offset] |= pixdata;
+			buf[x_offset + width] |= pixdata >> 16;
+			x_offset++;
+		}
+	}
+}
+
+// Converts a string to a 2bpp Nx16 image, dst must point to a buffer of size N * 4
+void string_to_img(void *dst, const u8 width, const char *str, bool centered) {
+	u8 start_x, pix_strlen = 0;
+
+	// Calculate string length
+	for (u8 i = 0; str[i]; i++)
+		pix_strlen += font_width[str[i] - 0x20];
+	start_x = centered ? (width - pix_strlen) / 2 : 2;
+
+	// Each column needs 4 bytes to encode 16 pixels
+	memset(dst, 0, width * 4);
+	render_string(dst, width, str, start_x + 1, 0, 1);
+	render_string(dst, width, str, start_x + 1, 1, 1);
+	render_string(dst, width, str, start_x, 1, 1);
+	render_string(dst, width, str, start_x, 0, 3);
 }
